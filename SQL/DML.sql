6cb21b6f23418112b4b51fb8bb2115d85caef77d
yasminhersi <143907206+yasminhersi@users.noreply.github.com>
2024-04-02 21:32:43 -0400

    Update DML.sql

--- SQL/DML.sql
+++ SQL/DML.sql
@@ -1,1 +1,26 @@
+CREATE TABLE members (
+    member_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+  	achieved_date DATE
+  	weight NUMBER
+  	height 
+  	goal_weight	
+);
 
+CREATE TABLE trainers (
+    trainer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+  	available DATE
+	
+	
+	
+);
+
+
+CREATE TABLE staff (
+    staff_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+	  available DATE
+	
+);
