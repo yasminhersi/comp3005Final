c0faa1aa70d25147c9cdb8e3412d23640539e5df
yasminhersi <143907206+yasminhersi@users.noreply.github.com>
2024-04-02 23:30:44 -0400

    Update DDL.sql

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,1 +1,4 @@
-
+INSERT INTO members (member_id, first_name, last_name, achieved_date, weight, height, goal_weight)
+VALUES 
+(1, 'John', 'Doe', '2024-05-01', 150, 170, 140),
+(2, 'Jane', 'Smith', '2024-08-01', 120, 150, 170);
