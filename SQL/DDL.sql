5bc56e39c3060012ef741d3806e8c3e3cf9beec5
yasminhersi <143907206+yasminhersi@users.noreply.github.com>
2024-04-06 18:39:10 -0400

    Update DDL.sql

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -1,4 +1,4 @@
-INSERT INTO members (member_id, first_name, last_name, achieved_date, weight, height, goal_weight)
+INSERT INTO members (member_id, first_name, last_name, achieved_date, curr_weight, goal_weight, height, username, password)
 VALUES 
-(1, 'John', 'Doe', '2024-05-01', 150, 170, 140),
-(2, 'Jane', 'Smith', '2024-08-01', 120, 150, 170);
+(1, 'John', 'Doe', '2024-05-01', 150, 170, 140, 'yasmin', '123'),
+(2, 'Smith', 'jane', '2024-08-01', 120, 150, 170, 'fatima', '123');
