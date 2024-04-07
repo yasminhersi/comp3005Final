70d1b8db37072bb53c4c8141c1e59ed31d46eb38
fatimaferdous <101489576+fatimaferdous@users.noreply.github.com>
2024-04-07 19:25:22 -0400

    Update application.py

--- application.py
+++ application.py
@@ -40,3 +40,37 @@ def getMemberInfo():
             print("Member not found.")
             
 getMemberInfo()
+
+def username_exists(username):
+    cur.execute("SELECT COUNT(*) FROM members WHERE username = %s", (username,))
+    count = cur.fetchone()[0]
+    return count > 0
+
+def register_member():
+    print("To register please enter the following information: ")
+    while True:
+        username = input("Username: ")
+        if username_exists(username):
+            print("Username already exists. Please choose a different one.")
+        else:
+            break
+
+    password = input("Password: ")
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+    weight = int(input("Weight (lbs): "))
+    height = int(input("Height (cm): "))
+    fitness_goal = int(input("Fitness Goal: "))
+    achievement_date = input("Please insert the date you would like to achieve your fitness goal by (year-month-day): ")
+
+    cur.execute("SELECT MAX(member_id) FROM members")
+    max_id = cur.fetchone()[0]
+    if max_id is None:
+        member_id = 1
+    else:
+        member_id = max_id + 1
+
+    cur.execute("INSERT INTO members (member_id, first_name, last_name, achieved_date, curr_weight, goal_weight, height, username, password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                   (member_id, first_name, last_name, achievement_date, weight, fitness_goal, height, username, password))
+    
+    print("You have been registered successfully!")
