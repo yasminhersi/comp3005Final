fa30c2bbdcb2ee3c08442b8b066b967eaa55e9c5
yasminhersi <143907206+yasminhersi@users.noreply.github.com>
2024-04-08 00:21:14 -0400

    added classes table

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -2,3 +2,13 @@ INSERT INTO members (member_id, first_name, last_name, achieved_date, curr_weigh
 VALUES 
 (1, 'John', 'Doe', '2024-05-01', 150, 170, 'to be ...', 'yasmin', '123'),
 (2, 'Smith', 'jane', '2024-08-01', 120, 150, 'to be ....', 'fatima', '123');
+
+INSERT INTO personal_classes (personal_classes_id, name, duration, available, trainer_id)
+VALUES 
+(1, 'HIIT', 30, '2024-04-12', NULL),
+(2, 'Cardio', 45, '2024-04-13', NULL);
+
+INSERT INTO group_classes (group_classes_id, name, duration, available, trainer_id)
+VALUES 
+(1, 'Pilates', 30, '2024, 4, 10', NULL),
+(2, 'Spinning', 45, '2024, 4, 11', NULL);
