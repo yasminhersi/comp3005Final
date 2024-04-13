18decf0471a523545d6d87c27f6fc9a8036be575
yasminhersi <143907206+yasminhersi@users.noreply.github.com>
2024-04-13 18:40:41 -0400

    Update DDL.sql

--- SQL/DDL.sql
+++ SQL/DDL.sql
@@ -4,15 +4,15 @@ VALUES
 (2, 'Smith', 'jane', '2024-08-01', 'to gain 10 lbs of muscle', 120, 150, 'fatima', '123');
 
 
-INSERT INTO personal_classes (personal_classes_id, name, duration, available, trainer_id, member_id)
+INSERT INTO personal_classes (personal_classes_id, name, duration, available, trainer_id, member_id, price)
 VALUES 
-(1, 'HIIT', 30, '2024-04-08 10:00:00', NULL, NULL),
-(2, 'Cardio', 45, '2024-04-18 12:00:00', NULL, NULL);
+(1, 'HIIT', 30, '2024-04-08 10:00:00', NULL, NULL, 20),
+(2, 'Cardio', 45, '2024-04-18 12:00:00', NULL, NULL, 10);
 
-INSERT INTO group_classes (group_classes_id, name, duration, time, trainer_id, members_count, max_members)
+INSERT INTO group_classes (group_classes_id, name, duration, time, trainer_id, members_count, max_members, price)
 VALUES 
-(1, 'Pilates', 30, '2024-04-10 16:00:00', NULL, NULL, 4),
-(2, 'Spinning', 45, '2024-04-10 19:00:00', NULL, NULL, 3);
+(1, 'Pilates', 30, '2024-04-10 16:00:00', NULL, NULL, 4, 5),
+(2, 'Spinning', 45, '2024-04-10 19:00:00', NULL, NULL, 3, 8);
 
 INSERT INTO exercises (exercises_id, name, sets, description, muscle_group, difficulty_level, minutes)
 VALUES
