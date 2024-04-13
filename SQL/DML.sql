a60a60858f71fcaa3305a77442a10315caf76bb2
yasminhersi <143907206+yasminhersi@users.noreply.github.com>
2024-04-12 21:07:24 -0400

    Update DML.sql

--- SQL/DML.sql
+++ SQL/DML.sql
@@ -50,7 +50,19 @@ CREATE TABLE equipments (
 CREATE TABLE exercises (
     exercises_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    number_sets INTEGER
+    sets INTEGER,
+	description TEXT,
+	muscle_group VARCHAR(255) NOT NULL,
+	difficulty_level INTEGER,
+	minutes INTEGER
 );
-
+CREATE TABLE health_statistics (
+    health_id SERIAL PRIMARY KEY,
+    member_id INTEGER NOT NULL,
+    weight INTEGER,
+	height VARCHAR(255) NOT NULL,
+	BMI INTEGER,
+	resting_heart INTEGER,
+    FOREIGN KEY (member_id) REFERENCES members(member_id)
+);
 #create a room bookings table
